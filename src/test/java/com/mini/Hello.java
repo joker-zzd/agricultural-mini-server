7fbbf6a68915a854752cf5710182260a128795b8
huangxianjun <y2638582465@gmai.com>
2026-03-01 23:42:33 +0800

    feat: test冲突

--- src/test/java/com/mini/Hello.java
+++ src/test/java/com/mini/Hello.java
@@ -2,6 +2,6 @@ package com.mini;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello W11orld!");
     }
 }
